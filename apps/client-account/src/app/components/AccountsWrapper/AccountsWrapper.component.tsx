1c8927746d8be222721d69eee5eacc3352b0ae10
Taras Pavlovskyy <Tpavlovskyy@wethaq.io>
2023-07-19 19:39:33 +0300

    feat: implement conditional rendering

--- apps/client-account/src/app/components/AccountsWrapper/AccountsWrapper.component.tsx
+++ apps/client-account/src/app/components/AccountsWrapper/AccountsWrapper.component.tsx
@@ -17,40 +17,46 @@ export const AccountsWrapperComponent: FC<IAccountsWrapperProps> = ({ children }
       label: "User Details",
       key: "user-details",
       paths: [routes.account.userDetails],
-      notification: 0,
+      notification: 0
     },
     {
       label: "Account Security",
       key: "account-security",
       paths: [routes.account.accountSecurity],
-      notification: 0,
+      notification: 0
     },
     {
       label: "Platform Access",
       key: "platform-access",
       paths: [routes.account.platformAccess],
-      notification: 0,
+      notification: 0
     },
     {
       label: "Data Room",
       key: "dataroom",
       paths: [routes.account.dataRoom],
-      notification: 0,
+      notification: 0
     },
     {
       label: "Onboard User",
       key: "onboard",
       paths: [routes.account.onboardUser],
       notification: 0,
-    },
+      roles: ["invst_mngr", "manager", "admin"]//List of allowed roles. Empry  == allow all
+    }
   ];
-
   const value = useMatchedPath(primariesTabs);
   const { user } = useUser();
+
+  const filteredTabs = primariesTabs.filter(tab => {
+    if (!tab.roles || tab.roles.length === 0) return true;
+    return !!tab.roles.includes(user?.role ?? "");
+  });
+
   return (
     <Styles.Accounts>
       <Tabs value={value}>
-        {primariesTabs.map((tab) => (
+        {filteredTabs.map((tab) => (
           <Tab value={tab.key} as={Link} to={tab.paths[0]} key={tab.key}>
             {tab.label}
             {tab.notification > 0 && <Badge>{tab.notification}</Badge>}
