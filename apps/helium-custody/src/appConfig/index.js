d27c22344833051723506c47e1487f77f7dec4b1
Moncy Gopalan <moncy@wethaq.io>
2023-08-11 15:52:04 +0530

    patch: fix helium app configs

--- apps/helium-custody/src/appConfig/index.js
+++ apps/helium-custody/src/appConfig/index.js
@@ -9,7 +9,7 @@ const parseFeatureFlags = (value) => {
 const generateAppConfig = (environment) => ({
   appENV: environment.VITE_APP_ENV,
   appRegion: environment.VITE_APP_REGION,
-  baseAPIURL: environment.VITE_APP_BASE_API_URL,
+  baseAPIURL: environment.VITE_API_URL,
   appEnable2FA: environment.VITE_APP_ENABLE_2FA,
   featureFlags: parseFeatureFlags(environment.VITE_APP_FEATURE_FLAGS),
 });
