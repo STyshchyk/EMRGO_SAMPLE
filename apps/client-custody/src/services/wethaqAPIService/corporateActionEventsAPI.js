c042ac435daea5910e101ee9851e58b54544d99e
Umar Maqbool <umarmaqbool37@gmail.com>
2023-08-18 13:14:15 +0500

    corporate action event prefix

--- apps/client-custody/src/services/wethaqAPIService/corporateActionEventsAPI.js
+++ apps/client-custody/src/services/wethaqAPIService/corporateActionEventsAPI.js
@@ -3,26 +3,26 @@ import { baseAxiosInstance } from "./helpers";
 const getCorporateActionEventsList = () =>
   baseAxiosInstance({
     method: "GET",
-    url: `/v1/externalSecurities/corporateEvents`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents`,
   });
 
 const cancelCorporateActionEvent = (payload) =>
   baseAxiosInstance({
     method: "PATCH",
-    url: `/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}/cancel`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}/cancel`,
   });
 
 const addCorporateActionEvent = (payload) =>
   baseAxiosInstance({
     method: "POST",
-    url: `/v1/externalSecurities/corporateEvents`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents`,
     data: payload.requestPayload,
   });
 
 const editCorporateActionEvent = (payload) =>
   baseAxiosInstance({
     method: "PATCH",
-    url: `/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}`,
     data: payload.requestPayload,
   });
 
@@ -30,13 +30,13 @@ const submitCAEventClientResponse = (payload) =>
   baseAxiosInstance({
     method: "POST",
     data: payload.requestPayload,
-    url: `/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}/response`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}/response`,
   });
 
 const getCorporationActionEvent = (payload) =>
   baseAxiosInstance({
     method: "GET",
-    url: `/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}`,
+    url: `/externalSecurities/v1/externalSecurities/corporateEvents/${payload.corporateActionEventId}`,
   });
 
 const corporateActionEventsAPI = {
