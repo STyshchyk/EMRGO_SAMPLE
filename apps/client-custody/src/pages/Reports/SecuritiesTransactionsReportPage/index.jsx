cb66230651a9496c4b39f8d43ff59121eab9fbea
TPavl <Tpavlovskyy@wethaq.io>
2023-08-21 12:27:23 +0300

    fix: bug id 131 client

--- apps/client-custody/src/pages/Reports/SecuritiesTransactionsReportPage/index.jsx
+++ apps/client-custody/src/pages/Reports/SecuritiesTransactionsReportPage/index.jsx
@@ -54,7 +54,7 @@ const SecuritiesTransactionsReportPage = () => {
   // const navigate = useNavigate();
   const tableRef = useRef();
   const mtableLocalization = useMaterialTableLocalization();
-  const { t } = useTranslation(["reports", "blotter", "filters"]);
+  const { t } = useTranslation(["reports", "blotter"]);
   const childRef = useRef();
 
   const [isAllEntitiesOptionSelected, setIsAllEntitiesOptionSelected] = useState(false);
