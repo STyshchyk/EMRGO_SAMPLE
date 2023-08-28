402a2054efd4788100dcd62a835227f112279ad6
TPavl <Tpavlovskyy@wethaq.io>
2023-08-28 13:53:08 +0300

    fix: handle empty user settings object

--- apps/helium-custody/src/redux/selectors/miscellaneous.js
+++ apps/helium-custody/src/redux/selectors/miscellaneous.js
@@ -9,7 +9,7 @@ const selectAllTableConfig = (state) => state.miscellaneous.tableConfig;
 const selectTableConfig = createSelector(
   [selectAllTableConfig, (state, tableKey) => tableKey],
   // Output selector gets (`items, category)` as args
-  (items, tableKey) => items.find((item) => item.key === tableKey)
+  (items, tableKey) => items && items.find((item) => item.key === tableKey) || null
 );
 
 export default selectDocumentLink;
