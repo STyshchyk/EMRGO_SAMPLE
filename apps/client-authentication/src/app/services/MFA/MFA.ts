6cc565b1450525f8acda9a8fef2796df3033c60d
TPavl <Tpavlovskyy@wethaq.io>
2023-08-29 13:15:04 +0300

    fix: correct mfa url

--- apps/client-authentication/src/app/services/MFA/MFA.ts
+++ apps/client-authentication/src/app/services/MFA/MFA.ts
@@ -27,7 +27,7 @@ export const verifyMFA = (requestObject: { code: string }) => {
   const promise = authApi({
     method: "POST",
     data: requestObject,
-    url: `/auth/v2/mfa/enable`,
+    url: `/auth/v2/mfa/verify`,
   });
   return promise;
 };
