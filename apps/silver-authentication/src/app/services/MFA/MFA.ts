925ca92140b876e26d023ac1d45ff8af08807f5a
TPavl <Tpavlovskyy@wethaq.io>
2023-08-29 15:00:51 +0300

    fix: otp payload fix

--- apps/silver-authentication/src/app/services/MFA/MFA.ts
+++ apps/silver-authentication/src/app/services/MFA/MFA.ts
@@ -1,33 +1,34 @@
 import { authApi } from "../APIService";
 import { IMFA } from "./MFA.types";
-import { ILoginCode } from "../../pages/Login/Login.types";
 
-export const enableMFA = (code: string) => {
-  return authApi({
+
+
+export const enableMFA = (requestObject: { code: string }) => {
+  const promise = authApi({
     method: "POST",
-    url: "auth/v2/mfa/enable",
-    validateStatus: null,
-    data: {
-      code,
-    },
+    data: requestObject,
+    url: `/auth/v2/mfa/enable`,
   });
+  return promise;
 };
 
 export const setupMFA = async (): Promise<IMFA> => {
-  const promies = authApi({
+  const promise = authApi({
     method: "POST",
     url: "auth/v2/mfa/setup",
-    validateStatus: null,
+    validateStatus: null
+
   });
-  const res = await (await promies).data;
+  const res = await (await promise).data;
   return res || [];
 };
 
-export const verifyMFA = (code: ILoginCode) => {
-  return authApi({
+export const verifyMFA = (requestObject: { code: string }) => {
+  const promise = authApi({
     method: "POST",
-    url: "auth/v2/mfa/verify",
-    validateStatus: null,
-    data: code
+    data: requestObject,
+    url: `/auth/v2/mfa/verify`,
   });
+  return promise;
 };
+
