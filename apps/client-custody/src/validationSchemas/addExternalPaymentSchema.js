6c2ccaa73c01743ae0e81d4668c4cb603d5ab855
TPavl <tpavlovskyy@wethaq.io>
2023-09-12 16:45:24 +0300

    fix: bug id 647 EX

--- apps/client-custody/src/validationSchemas/addExternalPaymentSchema.js
+++ apps/client-custody/src/validationSchemas/addExternalPaymentSchema.js
@@ -1,15 +1,11 @@
 import * as Yup from "yup";
 
 const addExternalPaymentSchema = Yup.object().shape({
-    sourceEntity: Yup.object().nullable().required('Source entity is required'),
-    sourceAccount: Yup.object().nullable().required('Source account is required'),
-    paymentAccount: Yup.object().nullable().required('Payment account is required'),
-    paymentAmount: Yup.string().nullable().required('Payment amount is required'),
-    paymentDetails: Yup.string().nullable(),
-    valueDate: Yup.date()
-    .nullable()
-    .required("Please select a value date"),
-    transferPurpose: Yup.object().nullable().required('Transfer purpose is required'),
+  paymentAccount: Yup.object().nullable().required("Payment account is required"),
+  paymentAmount: Yup.string().nullable().required("Payment amount is required"),
+  paymentDetails: Yup.string().nullable(),
+  valueDate: Yup.date().nullable().required("Please select a value date"),
+  transferPurpose: Yup.object().nullable().required("Transfer purpose is required"),
 });
 
 export default addExternalPaymentSchema;
