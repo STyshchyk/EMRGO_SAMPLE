2655b834f8c26f2f1ebe2dcd8b21335b8da56226
TPavl <tpavlovskyy@wethaq.io>
2023-09-12 13:40:56 +0300

    fix: improve onboard user ui

--- apps/client-account/src/app/pages/OnboardUser/OnboardUserModal/OnboardUser.schema.ts
+++ apps/client-account/src/app/pages/OnboardUser/OnboardUserModal/OnboardUser.schema.ts
@@ -2,11 +2,11 @@ import * as Yup from "yup";
 
 export const onboardUserSchema = Yup.object().shape({
   firstName: Yup.string()
-    .min(4, "First Name should contain more than 4 symbols")
+    .min(4, "First Name should be 4 or more symbols")
     .max(50, "First Name is Too Long!")
     .required("First Name is Required"),
   lastName: Yup.string()
-    .min(4, "Last Name should contain more than 4 symbols")
+    .min(4, "Last Name should be 4 or more symbols")
     .max(50, "Last Name is Too Long!")
     .required("Last Name is Required"),
   email: Yup.string().email("Enter valid email").required("Email ID is Required"),
