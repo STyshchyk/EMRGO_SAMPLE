d72cdc75186821431c92c1fc837b9898fdadaf8f
TPavl <tpavlovskyy@wethaq.io>
2023-11-06 12:02:52 +0200

    fix: bug id 1174 IN

--- apps/helium-custody/src/pages/OperationsSecServices/Registrar/index.jsx
+++ apps/helium-custody/src/pages/OperationsSecServices/Registrar/index.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import LoadingPage from "../../../components/LoadingPage";
 import SecuritiesHoldingsTable, {
@@ -7,23 +9,38 @@ import SecuritiesHoldingsTable, {
 } from "../../../components/SecuritiesHoldingsTable";
 import useWethaqAPIParams from "../../../hooks/useWethaqAPIParams";
 import * as reportsActionCreators from "../../../redux/actionCreators/reports";
+import * as safekeepingActionCreators from "../../../redux/actionCreators/safekeeping";
 import * as authSelectors from "../../../redux/selectors/auth";
 import * as reportsSelectors from "../../../redux/selectors/reports";
+import * as safekeepingSelectors from "../../../redux/selectors/safekeeping";
 
 const Registrar = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { t } = useTranslation(["reports"]);
+
+  const [isTradeDateHolding, setIsTradeDateHolding] = useState(false);
 
   // selectors
   const currentEntityGroup = useSelector(authSelectors.selectCurrentEntityGroup);
-  const currentEntityGroupID = currentEntityGroup?.id;
-
+  const currentEntityType = useSelector(authSelectors.selectCurrentEntityType);
   const securitiesAccounts = useSelector(reportsSelectors.selectSecuritiesAccounts);
-  const securitiesHoldingsData = useSelector(reportsSelectors.selectSecuritiesHoldingsData);
-  const isFetching = useSelector(reportsSelectors.selectIsFetching);
+  const safekeepingAccounts = useSelector(safekeepingSelectors.readAccounts);
 
-  const generatedTableData = securitiesHoldingsData?.map((i) =>
-    generateSecuritiesHoldingsTableRowData(i)
+  const settlementDatedSecuritiesHoldingsData = useSelector(
+    reportsSelectors.selectSecuritiesHoldingsData
   );
+  const tradeDatedSecuritiesHoldingsData = useSelector(
+    reportsSelectors.selectTradeDatedSecuritiesHoldingsData
+  );
+  const isFetching = useSelector(reportsSelectors.selectIsFetching);
+  const securitiesHoldingsData = isTradeDateHolding
+    ? tradeDatedSecuritiesHoldingsData
+    : settlementDatedSecuritiesHoldingsData;
+  const currentEntityGroupID = currentEntityGroup?.id;
+  const generatedTableData = securitiesHoldingsData
+    ?.map((i) => generateSecuritiesHoldingsTableRowData(i))
+    .filter((row) => row.quantity !== "--");
 
   useWethaqAPIParams({
     currentGroupId: currentEntityGroupID,
@@ -33,7 +50,12 @@ const Registrar = () => {
     const fetchSecuritiesAccounts = (payload) =>
       dispatch(reportsActionCreators.doFetchSecuritiesAccounts(payload));
 
+    const fetchSafekeepingAccounts = (payload) =>
+      dispatch(safekeepingActionCreators.doReadAccounts(payload));
+
     fetchSecuritiesAccounts();
+
+    fetchSafekeepingAccounts();
 
     return () => {
       dispatch(reportsActionCreators.doResetSecuritiesHoldings());
@@ -47,6 +69,10 @@ const Registrar = () => {
       data={generatedTableData}
       securitiesAccounts={securitiesAccounts}
       disableDateFilter
+      safekeepingAccounts={safekeepingAccounts}
+      entityUserType={currentEntityType}
+      isTradeDateHolding={isTradeDateHolding}
+      setIsTradeDateHolding={setIsTradeDateHolding}
     />
   );
 };
