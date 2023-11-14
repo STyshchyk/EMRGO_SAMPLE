325a40e2056fa4e1142281fceca166778577326c
TPavl <tpavlovskyy@wethaq.io>
2023-11-14 11:56:05 +0200

    feat: redo pagination

--- packages/shared-ui/src/lib/Table/Table.tsx
+++ packages/shared-ui/src/lib/Table/Table.tsx
@@ -1,10 +1,6 @@
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import LastPageIcon from "@mui/icons-material/LastPage";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import Tooltip from "@mui/material/Tooltip";
+import React from "react";
+
+import TablePagination from "@mui/material/TablePagination";
 import { flexRender } from "@tanstack/react-table";
 
 import { ChevronDownIcon, ChevronUpIcon } from "../Icons";
@@ -17,6 +13,21 @@ export const Table = function <T>({
   displayShown,
   pagination = false,
 }: ITableProps<T>) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    table.setPageIndex(newPage);
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    table.setPageSize(Number(parseInt(event.target.value, 10)));
+    setPage(0);
+  };
   return (
     <>
       <Styles.Table>
@@ -63,104 +74,19 @@ export const Table = function <T>({
           ))}
         </tbody>
       </Styles.Table>
-
       {pagination && (
-        <div className="flex sm:flex-row flex-col w-full mt-2 items-center gap-5 text-xs justify-end">
-          <div className="mb1">
-            <span className="mr-2">Rows per page:</span>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={table.getState().pagination.pageSize}
-              label="Number"
-              style={{ padding: "1px" }}
-              sx={{
-                boxShadow: "none",
-                ".MuiOutlinedInput-notchedOutline": { border: 0 },
-                "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-                  border: 0,
-                },
-                "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: 0,
-                },
-              }}
-              onChange={(e) => {
-                table.setPageSize(Number(e.target.value));
-              }}
-            >
-              {[5, 10, 15, 20].map((pageSize) => (
-                <MenuItem key={pageSize} value={pageSize}>
-                  {pageSize}
-                </MenuItem>
-              ))}
-            </Select>
-          </div>
-          <div className="flex gap-2 items-center">
-            <button
-              style={{ all: "unset" }}
-              onClick={() => table.setPageIndex(0)}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <Tooltip title={"First page"}>
-                <FirstPageIcon
-                  className={`${
-                    !table.getCanPreviousPage()
-                      ? "text-slate-500"
-                      : "hover:bg-gray-100 hover:cursor-pointer"
-                  } rounded p-1`}
-                />
-              </Tooltip>
-            </button>
-            <button
-              style={{ all: "unset" }}
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <Tooltip title={"Previous page"}>
-                <NavigateBeforeIcon
-                  className={`${
-                    !table.getCanPreviousPage()
-                      ? "text-slate-500"
-                      : "hover:bg-gray-100 hover:cursor-pointer"
-                  } rounded p-1`}
-                />
-              </Tooltip>
-            </button>
-            <span style={{ marginBottom: "4px" }}>
-              {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-            </span>
-            <button
-              style={{ all: "unset" }}
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              <Tooltip title={"Next page"}>
-                <NavigateNextIcon
-                  className={`${
-                    !table.getCanNextPage()
-                      ? "text-slate-500"
-                      : "hover:bg-gray-100 hover:cursor-pointer  "
-                  } rounded p-1`}
-                />
-              </Tooltip>
-            </button>
-            <button
-              style={{ all: "unset" }}
-              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-              disabled={!table.getCanNextPage()}
-            >
-              <Tooltip title={"Last page"}>
-                <LastPageIcon
-                  className={`${
-                    !table.getCanNextPage()
-                      ? "text-slate-500"
-                      : "hover:bg-gray-100 hover:cursor-pointer  "
-                  } rounded p-1`}
-                />
-              </Tooltip>
-            </button>
-          </div>
-        </div>
+        <TablePagination
+          className="mt-2"
+          component="div"
+          count={table.getFilteredRowModel().rows.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[5, 10, 15, 20]}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          showFirstButton
+          showLastButton
+        />
       )}
     </>
   );
