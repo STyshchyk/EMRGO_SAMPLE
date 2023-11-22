0a1dd237af043300ef4eaa8198bee15d7798f7e5
TPavl <tpavlovskyy@wethaq.io>
2023-11-22 11:07:30 +0200

    fix: EMBE 1280

--- apps/helium-custody/src/components/SecuritiesTradesTable/index.jsx
+++ apps/helium-custody/src/components/SecuritiesTradesTable/index.jsx
@@ -648,7 +648,7 @@ const SecurityTradesTable = ({
                 </Grid>
 
                 <Grid item xs={12} md={6} lg={3}>
-                  <ExportButtons tableRef={tableRef} name="‘Settlement Transaction Report" />
+                  <ExportButtons tableRef={tableRef} name="Settlement Transaction Report" />
                 </Grid>
               </Grid>
             </TableFiltersWrapper>
