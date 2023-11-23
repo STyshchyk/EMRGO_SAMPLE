27f679f49edce0734af13fa785a230025de908d6
TPavl <tpavlovskyy@wethaq.io>
2023-11-23 14:09:16 +0200

    fix: 1274 EMBE

--- apps/helium-custody/src/redux/selectors/reports.js
+++ apps/helium-custody/src/redux/selectors/reports.js
@@ -62,14 +62,25 @@ export const selectSecuritiesTransactions = createSelector(
   [selectSecuritiesTransactionsData],
   (transactionsData) => {
     // Is used to get a mutable array needed by material table
-    if (transactionsData) {
-      const finalData = [];
-      transactionsData.forEach((transaction) => {
-        finalData.push({ ...transaction });
+    if (transactionsData && Array.isArray(transactionsData)) {
+      let finalData = [];
+      finalData = transactionsData.map((transaction) => {
+        return {
+          ...transaction,
+          fromSecurityAccount:
+            ["DVP", "DFOP"].includes(transaction.settlementType.name) &&
+            transaction.investorName.includes("Emrgo")
+              ? transaction.toSecurityAccount
+              : transaction.fromSecurityAccount,
+          investorName:
+            ["DVP", "DFOP"].includes(transaction.settlementType.name) &&
+            transaction.investorName.includes("Emrgo")
+              ? transaction.issuerName
+              : transaction.investorName,
+        };
       });
       return finalData;
     }
-    return [];
   }
 );
 
