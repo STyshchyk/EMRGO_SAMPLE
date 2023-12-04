a34ef08babbec9104e3b24f312c6e00d048798f9
smahmood22 <smahmood@wethaq.io>
2023-12-04 15:55:47 +0300

    fix: ebme-1498

--- apps/helium-custody/src/components/SecuritiesHoldingsTable/index.jsx
+++ apps/helium-custody/src/components/SecuritiesHoldingsTable/index.jsx
@@ -395,7 +395,7 @@ const SecuritiesHoldingsTable = ({
                 />
               </Grid>
               <Grid item xs={12} md={6} lg={3}>
-                <ExportButtons tableRef={tableRef} name="Security Registrar Report" />
+                <ExportButtons tableRef={tableRef} name="Security Holdings Report" />
               </Grid>
 
               {/* <Grid item xs={12} container>
