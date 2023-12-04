f18ba81d776b927fa0fc6c9614c1cbc0dc7fffcb
Glenn Fernandes <glennferns93@gmail.com>
2023-12-04 18:47:01 +0530

    Added source account selection when creating payment instruction

--- apps/client-custody/src/pages/CashManagement/PaymentInstructionsPage/AddPaymentInstructionForm/index.jsx
+++ apps/client-custody/src/pages/CashManagement/PaymentInstructionsPage/AddPaymentInstructionForm/index.jsx
@@ -40,189 +40,231 @@ const AddPaymentInstructionForm = ({
   } = options;
   // local states
 
-  const [selectedCurrencyName, setSelectedCurrencyName] = useState(
-    initialValues.paymentAccount?.value?.currency ?? ""
-  );
-  const [filteredSourceAccountOptions, setFilteredSourceAccountOptions] = useState(
-    allSourceAccountOptions.filter((i) => initialValues.sourceEntity?.value === i.value.entityId)
-  );
-  const [filteredPaymentAccountOptions, setFilteredPaymentAccountOptions] = useState(
-    isWethaqUser
-      ? allPaymentAccountOptions.filter(
-          (i) => initialValues.beneficiaryEntityGroupUser?.value?.id === i.value.userId
-        )
-      : allPaymentAccountOptions
-  );
+  const [filteredSourceAccountOptions, setFilteredSourceAccountOptions] =
+    useState(allSourceAccountOptions);
+  const [filteredPaymentAccountOptions, setFilteredPaymentAccountOptions] =
+    useState(allPaymentAccountOptions);
 
   return (
     <Formik
       initialValues={{
         ...initialValues,
-        paymentAccount:
-          filteredPaymentAccountOptions.lenght !== 0
-            ? filteredPaymentAccountOptions.filter((account) => account.value.isDefault)[0]
-            : null,
       }}
       onSubmit={handleSubmit}
       // validationSchema={addExternalPaymentSchema}
       enableReinitialize
     >
-      {({ values, setFieldValue, resetForm }) => (
-        <Form className={cx(style.formWrapper)}>
-          {initial && (
-            <AutoSaveFields formKey="AddPaymentInstructionDialogForm" initial={initial} />
-          )}
-          <Box mb={2}>
-            <Box my={1} className="w-full">
-              <Select
-                name="paymentAccount"
-                components={{ Option: ReactSelectCurrencyOption }}
-                closeMenuOnSelect
-                placeholder={t("Payment Instructions.Modals.Fields.Payment Account")}
-                isSearchable
-                styles={selectStyles}
-                value={values.paymentAccount}
-                isClearable
-                options={filteredPaymentAccountOptions}
-                onChange={(selectedOption, triggeredAction) => {
-                  if (triggeredAction.action === "clear") {
-                    setSelectedCurrencyName("");
-                    setFieldValue("paymentAccount", null);
-                  }
+      {({ values, setFieldValue, resetForm }) => {
+        
+        return (
+          <Form className={cx(style.formWrapper)}>
+            {/* {initial && (
+              <AutoSaveFields formKey="AddPaymentInstructionDialogForm" initial={initial} />
+            )} */}
+            <Box mb={2}>
+              <Box my={1} className="w-full">
+                <Select
+                  name="sourceAccount"
+                  placeholder={t("Payment Instructions.Modals.Placeholders.Source Account")}
+                  components={{ Option: ReactSelectCurrencyOption }}
+                  closeMenuOnSelect
+                  isSearchable
+                  styles={selectStyles}
+                  menuPortalTarget={document.body}
+                  value={values.sourceAccount}
+                  isClearable
+                  options={filteredSourceAccountOptions}
+                  onChange={(selectedOption, triggeredAction) => {
+                    console.log("🚀 ~ file: index.jsx:93 ~ selectedOption:", selectedOption);
+                    if (triggeredAction.action === "clear") {
+                      setFieldValue("sourceAccount", null);
+                    }
+                    if (triggeredAction.action === "select-option") {
+                      const modifiedOption = {
+                        ...selectedOption,
+                        label: selectedOption.customLabel,
+                      };
+                      setFieldValue("sourceAccount", modifiedOption);
 
-                  if (triggeredAction.action === "select-option") {
-                    setFieldValue("paymentAccount", selectedOption);
-                    setSelectedCurrencyName(selectedOption?.value?.currency);
-                  }
-                }}
-              />
-              <ErrorMessage
-                component={Typography}
-                variant="caption"
-                color="error"
-                className="ml-4"
-                name="paymentAccount"
-              />
-            </Box>
-            <Box my={1} className="w-full">
-              <Grid item xs={12}>
-                <Field
-                  fullWidth
-                  component={(props) => (
-                    <DatePicker
-                      {...props}
-                      sx={{
-                        width: "100%", // Ensure the DatePicker takes full-width
-                        ...(props.sx || {}),
-                      }}
-                    />
-                  )}
-                  minDate={moment()}
-                  onChange={(date) => {
-                    setFieldValue("valueDate", date);
-                  }}
-                  value={values.valueDate ? moment(values.valueDate) : null}
-                  format={"DD/MM/YYYY"}
-                  inputVariant="filled"
-                  label={t("Payment Instructions.Modals.Fields.Value Date")}
-                  name="valueDate"
-                  validateOnChange
-                  validate={(date) => {
-                    if (!date) {
-                      return "Please select a date";
+                      setFieldValue("paymentAccount", null);
+
+                      setFilteredPaymentAccountOptions(
+                        allPaymentAccountOptions.filter(
+                          (i) => i.value.currency === selectedOption.value.currency
+                        )
+                      );
                     }
                   }}
                 />
-              </Grid>
+                <ErrorMessage
+                  component={Typography}
+                  variant="caption"
+                  color="error"
+                  className="ml-4"
+                  name="sourceAccount"
+                />
+              </Box>
 
-              <ErrorMessage
-                component={Typography}
-                variant="caption"
-                color="error"
-                className="ml-4"
-                name="valueDate"
-              />
-            </Box>
-            <Box my={1} className="w-full">
-              <Grid item xs={12}>
-                <Field
-                  fullWidth
-                  component={TextField}
-                  label={t("Payment Instructions.Modals.Fields.Payment Amount")}
-                  name="paymentAmount"
-                  variant="outlined"
-                  InputProps={{
-                    inputComponent: CustomNumberInputField,
-                    endAdornment: (
-                      <InputAdornment position="end">{selectedCurrencyName}</InputAdornment>
-                    ),
-                  }}
-                  // eslint-disable-next-line  react/jsx-no-duplicate-props
-                  inputProps={{
-                    min: 0,
+              <Box my={1} className="w-full">
+                <Select
+                  name="paymentAccount"
+                  components={{ Option: ReactSelectCurrencyOption }}
+                  closeMenuOnSelect
+                  placeholder={t("Payment Instructions.Modals.Fields.Payment Account")}
+                  isSearchable
+                  styles={selectStyles}
+                  value={values.paymentAccount}
+                  isClearable
+                  options={filteredPaymentAccountOptions}
+                  onChange={(selectedOption, triggeredAction) => {
+                    if (triggeredAction.action === "clear") {
+                      setFieldValue("paymentAccount", null);
+                    }
+
+                    if (triggeredAction.action === "select-option") {
+                      setFieldValue("paymentAccount", selectedOption);
+                    }
                   }}
                 />
-              </Grid>
-            </Box>
-            <Box my={1} className="w-full">
-              <Field
-                fullWidth
-                multiline
-                rows={4}
-                component={TextField}
-                label={t("Payment Instructions.Modals.Fields.Payment Details")}
-                name="paymentDetails"
-                variant="filled"
-              />
-            </Box>
-            <Box my={1} className="w-full">
-              <Select
-                name="transferPurpose"
-                closeMenuOnSelect
-                placeholder={t("Payment Instructions.Modals.Placeholders.Transfer Purpose")}
-                isSearchable
-                styles={selectStyles}
-                menuPortalTarget={document.body}
-                value={values.transferPurpose}
-                isClearable
-                options={paymentTransferPurposeOptions}
-                onChange={(selectedOption) => {
-                  setFieldValue("transferPurpose", selectedOption);
-                }}
-              />
-              <ErrorMessage
-                component={Typography}
-                variant="caption"
-                color="error"
-                className="ml-4"
-                name="transferPurpose"
-              />
-            </Box>
+                <ErrorMessage
+                  component={Typography}
+                  variant="caption"
+                  color="error"
+                  className="ml-4"
+                  name="paymentAccount"
+                />
+              </Box>
+              <Box my={1} className="w-full">
+                <Grid item xs={12}>
+                  <Field
+                    fullWidth
+                    component={(props) => (
+                      <DatePicker
+                        {...props}
+                        sx={{
+                          width: "100%", // Ensure the DatePicker takes full-width
+                          ...(props.sx || {}),
+                        }}
+                      />
+                    )}
+                    minDate={moment()}
+                    onChange={(date) => {
+                      setFieldValue("valueDate", date);
+                    }}
+                    value={values.valueDate ? moment(values.valueDate) : null}
+                    format={"DD/MM/YYYY"}
+                    inputVariant="filled"
+                    label={t("Payment Instructions.Modals.Fields.Value Date")}
+                    name="valueDate"
+                    validateOnChange
+                    validate={(date) => {
+                      if (!date) {
+                        return "Please select a date";
+                      }
+                    }}
+                  />
+                </Grid>
 
-            <Box my={2} className="w-full">
-              <Grid container xs={12} justifyContent="flex-end">
-                <Button
-                  onClick={() => {
-                    resetForm();
-                    handleCancel();
+                <ErrorMessage
+                  component={Typography}
+                  variant="caption"
+                  color="error"
+                  className="ml-4"
+                  name="valueDate"
+                />
+              </Box>
+              <Box my={1} className="w-full">
+                <Grid item xs={12}>
+                  <Field
+                    fullWidth
+                    component={TextField}
+                    label={t("Payment Instructions.Modals.Fields.Payment Amount")}
+                    name="paymentAmount"
+                    variant="filled"
+                    type="number"
+                    min="0"
+                    onChange={(event) => {
+                      const regexTwoDecimal = /^[0-9]*(\.[0-9]{0,2})?$/;
+                      if (regexTwoDecimal.test(event.target.value)) {
+                        setFieldValue("paymentAmount", event.target.value);
+                      }
+                    }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Typography color="primary" variant="subtitle2">
+                            {values.paymentAccount?.value?.currency}
+                          </Typography>
+                        </InputAdornment>
+                      ),
+                    }}
+                    // eslint-disable-next-line  react/jsx-no-duplicate-props
+                    inputProps={{
+                      min: 0,
+                    }}
+                  />
+                </Grid>
+              </Box>
+              <Box my={1} className="w-full">
+                <Field
+                  fullWidth
+                  multiline
+                  rows={4}
+                  component={TextField}
+                  label={t("Payment Instructions.Modals.Fields.Payment Details")}
+                  name="paymentDetails"
+                  variant="filled"
+                />
+              </Box>
+              <Box my={1} className="w-full">
+                <Select
+                  name="transferPurpose"
+                  closeMenuOnSelect
+                  placeholder={t("Payment Instructions.Modals.Placeholders.Transfer Purpose")}
+                  isSearchable
+                  styles={selectStyles}
+                  menuPortalTarget={document.body}
+                  value={values.transferPurpose}
+                  isClearable
+                  options={paymentTransferPurposeOptions}
+                  onChange={(selectedOption) => {
+                    setFieldValue("transferPurpose", selectedOption);
                   }}
-                  color="primary"
-                >
-                  {t("Entity Accounts.Cancel")}
-                </Button>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  disabled={!values?.valueDate || isSubmitting}
-                >
-                  {t("Entity Accounts.Submit")}
-                </Button>
-              </Grid>
+                />
+                <ErrorMessage
+                  component={Typography}
+                  variant="caption"
+                  color="error"
+                  className="ml-4"
+                  name="transferPurpose"
+                />
+              </Box>
+
+              <Box my={2} className="w-full">
+                <Grid container xs={12} justifyContent="flex-end">
+                  <Button
+                    onClick={() => {
+                      resetForm();
+                      handleCancel();
+                    }}
+                    color="primary"
+                  >
+                    {t("Entity Accounts.Cancel")}
+                  </Button>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={!values?.valueDate || isSubmitting}
+                  >
+                    {t("Entity Accounts.Submit")}
+                  </Button>
+                </Grid>
+              </Box>
             </Box>
-          </Box>
-        </Form>
-      )}
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
