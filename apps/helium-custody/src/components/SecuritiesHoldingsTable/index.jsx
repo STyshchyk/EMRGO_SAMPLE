a6d2995f4c204caa726afe7b765a5a52d8f3d832
smahmood22 <smahmood@wethaq.io>
2023-12-06 16:19:29 +0300

    fix: ebme-1513

--- apps/helium-custody/src/components/SecuritiesHoldingsTable/index.jsx
+++ apps/helium-custody/src/components/SecuritiesHoldingsTable/index.jsx
@@ -263,7 +263,7 @@ const SecuritiesHoldingsTable = ({
       id: "isin",
       title: "ISIN",
       field: "isin",
-      exportConfig: { width: 8 },
+      exportConfig: { width: 10 },
     },
     {
       id: "broker",
