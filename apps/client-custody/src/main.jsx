5cb9ee2df2873383779bf9c0664b6e232c6e6d38
Taras Pavlovskyy <tpavlovskyy@wethaq.io>
2023-12-29 10:56:37 +0000

    EBME-1934

--- apps/client-custody/src/main.jsx
+++ apps/client-custody/src/main.jsx
@@ -1,6 +1,9 @@
 import * as ReactDOM from "react-dom/client";
 
+import moment from "moment-timezone";
+
 import ConnectedApp from "./app/app";
 
+moment.tz.setDefault("Etc/UTC");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ConnectedApp />);
