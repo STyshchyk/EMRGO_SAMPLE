f0ceb667c1f52dc65cc5dce62b25183be1d43559
TPavl <tpavlovskyy@wethaq.io>
2024-01-08 15:13:01 +0200

    feat: normalize scroll bar

--- packages/shared-ui/src/lib/ScrollBar/Scroll.styles.ts
+++ packages/shared-ui/src/lib/ScrollBar/Scroll.styles.ts
@@ -3,20 +3,63 @@ import { css } from "styled-components";
 
 export const ScrollStyles = css`
   &::-webkit-scrollbar-track {
-    background-color: #f5f5f5;
+    ${(props) =>
+      props.theme.mode === "light" &&
+      css`
+        background-color: ${getTheme("colors.light")};
+      `}
+
+    ${(props) =>
+      props.theme.mode === "dark" &&
+      css`
+        background-color: ${getTheme("colors.dark")};
+      `}
   }
 
   &::-webkit-scrollbar {
-    height: 3px;
-    width: 3px;
-    background-color: ${getTheme("colors.green3")};
+    height: 8px;
+    width: 8px;
   }
-
-  scrollbar-color: ${getTheme("colors.green3")} #f5f5f5;
-  scrollbar-width: thin;
 
   &::-webkit-scrollbar-thumb {
-    background-color: ${getTheme("colors.green3")};
-    border: 1px solid ${getTheme("colors.green3")};
+    ${(props) =>
+      props.theme.mode === "light" &&
+      css`
+        background-color: ${getTheme("colors.green4")};
+        border: 1px solid ${getTheme("colors.dark")};
+
+        &:hover {
+          background-color: ${getTheme("colors.green3")};
+        }
+      `}
+
+    ${(props) =>
+      props.theme.mode === "dark" &&
+      css`
+        background-color: ${getTheme("colors.green4")};
+        border: 1px solid ${getTheme("colors.dark")};
+
+        &:hover {
+          background-color: ${getTheme("colors.green3")};
+        }
+      `}
+
+    border-radius: 3px;
+    padding: 1px;
   }
+
+  ${(props) =>
+    props.theme.mode === "light" &&
+    css`
+      scrollbar-color: ${getTheme("colors.green3")} ${getTheme("colors.light")};
+    `}
+
+  ${(props) =>
+    props.theme.mode === "dark" &&
+    css`
+      scrollbar-color: ${getTheme("colors.green3")} ${getTheme("colors.dark")};
+    `}
+
+
+  scrollbar-width: thin;
 `;
